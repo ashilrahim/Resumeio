3941ac51855e9720e6ab7585691e1efd4c6cf8ee
ashil rahim <ashilrahim2@gmail.com>
2025-09-06 15:25:41 +0530

    first commit

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,11 +3,20 @@ import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "AI Resumeio" },
+    { name: "description", content: "Welcome to AI Resumeio!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main className="bg-[url(/images/bg-main.svg)] bg-cover">
+      <section className="main-section">
+        <div className="page-heading">
+          <h1>Track your Application & Resume Ratings</h1>
+          <h1>Review your submission and check AI powered Feedback.</h1>
+        </div>
+      </section>
+    </main>
+  )
 }
